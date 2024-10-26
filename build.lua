a3f712acd7873fa08e5ccc2b15d87f829df5671f
Clea F. Rees <ReesC21@cardiff.ac.uk>
2024-10-26 03:41:49 +0100

    make summary fit ctan cap

--- build.lua
+++ build.lua
@@ -1,4 +1,4 @@
--- $Id: build.lua 10529 2024-10-25 21:19:36Z cfrees $
+-- $Id: build.lua 10532 2024-10-26 02:39:56Z cfrees $
 -- Build configuration for prooftrees
 --------------------------------------------------------------------------------
 os.setenv ("PATH", "/usr/local/texlive/bin:/usr/bin:")
@@ -29,7 +29,7 @@ uploadconfig = {
   uploader = "Clea F. Rees",
   license = "lppl1.3c",
   update = true,
-  summary = "Annotated logical tableaux based on forest with support for cross-references in justifications and closures. v0.9 adds out-of-the-box support for memoize.",
+  summary = "Annotated logical tableaux based on forest with support for cross-references and memoization.",
   description = "The package supports drawing proof trees of the kind often used in introductory logic classes, especially those aimed at students without strong mathematical backgrounds. Hodges (1991) is one example of a text which uses this system. When teaching such a system it is especially useful to annotate the tree with line numbers, justifications and explanations of branch closures.\n\nprooftrees provides a single environment, (either prooftree or tableau), and a variety of tools for annotating, customising and highlighting such trees. A cross-referencing system is provided for trees which cite line numbers in justifications for proof lines or branch closures.\n\nprooftrees is based on forest and, hence, TikZ. The package requires version 2.0.2 of Forest for expected results and will not work with version 1. Out-of-the-box support for memoization is based on forest version 2.1.",
 	bugtracker = "https://codeberg.org/cfr/prooftrees/issues",
 	repository = {"https://codeberg.org/cfr/prooftrees", "https://github.com/cfr42/prooftrees"},
