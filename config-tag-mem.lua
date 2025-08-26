bf1a79c35c14a7fb3b554244600897c41cf04e01
Clea F. Rees <cfr@noreply.codeberg.org>
2025-08-26 14:25:10 +0100

    TeX : prooftrees : add highly exp mem tag tests

---
+++ config-tag-mem.lua
@@ -0,0 +1,6 @@
+-- $Id: config-tag-mem.lua 11146 2025-08-26 13:23:52Z cfrees $
+-- Build configuration for prooftrees
+testfiledir = "testfiles-tag-mem"
+checkengines = { "luatex" }
+checkruns = 4
+-- vim: ts=2:sw=2:et:
