4dfcd33564e668ca35c6debb72a63efab69fbe87
Clea F. Rees <cfr@noreply.codeberg.org>
2025-08-30 05:25:17 +0100

    TeX : prooftrees : add standard tlgs for pdftex for tag testing config

--- config-tag.lua
+++ config-tag.lua
@@ -1,6 +1,6 @@
--- $Id: config-tag.lua 11142 2025-08-26 01:16:58Z cfrees $
+-- $Id: config-tag.lua 11165 2025-08-30 04:24:38Z cfrees $
 -- Build configuration for prooftrees
 testfiledir = "testfiles-tag"
-checkengines = { "luatex" }
+checkengines = { "pdftex", "luatex" }
 checkruns = 2
 -- vim: ts=2:sw=2:et:
