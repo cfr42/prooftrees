d9acf591b06a6e5a7d945667ebfc93b3416f9912
Clea F. Rees <cfr@noreply.codeberg.org>
2025-08-30 14:28:47 +0100

    TeX : prooftrees : try pdftex tests for config-tag-mem

--- config-tag-mem.lua
+++ config-tag-mem.lua
@@ -1,6 +1,6 @@
--- $Id: config-tag-mem.lua 11146 2025-08-26 13:23:52Z cfrees $
+-- $Id: config-tag-mem.lua 11166 2025-08-30 13:28:40Z cfrees $
 -- Build configuration for prooftrees
 testfiledir = "testfiles-tag-mem"
-checkengines = { "luatex" }
+checkengines = { "pdftex", "luatex" }
 checkruns = 4
 -- vim: ts=2:sw=2:et:
