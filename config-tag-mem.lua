807cf6033428e18e6359517529aaeeecee739207
Clea F. Rees <cfr@noreply.codeberg.org>
2025-09-04 01:52:26 +0100

    TeX : prooftrees : rhy ddrwg.

--- config-tag-mem.lua
+++ config-tag-mem.lua
@@ -1,6 +1,7 @@
--- $Id: config-tag-mem.lua 11166 2025-08-30 13:28:40Z cfrees $
+-- $Id: config-tag-mem.lua 11201 2025-09-04 00:52:06Z cfrees $
 -- Build configuration for prooftrees
 testfiledir = "testfiles-tag-mem"
-checkengines = { "pdftex", "luatex" }
+-- checkengines = { "pdftex", "luatex" }
+checkengines = { "luatex" }
 checkruns = 4
 -- vim: ts=2:sw=2:et:
