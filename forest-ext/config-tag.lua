95e71ab68854e610cfc67fbe44c829f50351eb33
Clea F. Rees <cfr@noreply.codeberg.org>
2026-01-14 21:53:15 +0000

    add files from forest-ext repo to sub-dir

---
+++ forest-ext/config-tag.lua
@@ -0,0 +1,9 @@
+-- $Id: config-tag.lua 11471 2026-01-14 21:49:31Z cfrees $
+-- Build configuration for prooftrees
+--------------------------------------------------------------------------------
+testfiledir = "testfiles-tag"
+checkengines = { "pdftex", "luatex" }
+checkruns = 3
+recordstatus = true
+--------------------------------------------------------------------------------
+-- vim: ts=2:sw=2:et:
