218085e21716ffc871270e3957661fd9c43fcfe4
Clea F. Rees <cfr@noreply.codeberg.org>
2026-01-14 21:21:38 +0000

    prooftrees reorg

---
+++ tableaux/config-tag-mem.lua
@@ -0,0 +1,7 @@
+-- $Id: config-tag-mem.lua 11201 2025-09-04 00:52:06Z cfrees $
+-- Build configuration for prooftrees
+testfiledir = "testfiles-tag-mem"
+-- checkengines = { "pdftex", "luatex" }
+checkengines = { "luatex" }
+checkruns = 4
+-- vim: ts=2:sw=2:et:
