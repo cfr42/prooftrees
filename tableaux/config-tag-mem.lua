3643c80737e66c91f780538aa9246d009fdcf918
Clea F. Rees <cfr@noreply.codeberg.org>
2026-01-18 22:13:43 +0000

    TeX : prooftrees : tableaux : update tag/mem stuff and use pdftex also for tag+mem tests

--- tableaux/config-tag-mem.lua
+++ tableaux/config-tag-mem.lua
@@ -1,7 +1,7 @@
--- $Id: config-tag-mem.lua 11470 2026-01-14 21:23:24Z cfrees $
+-- $Id: config-tag-mem.lua 11524 2026-01-18 22:12:49Z cfrees $
 -- Build configuration for prooftrees
 testfiledir = "testfiles-tag-mem"
--- checkengines = { "pdftex", "luatex" }
-checkengines = { "luatex" }
+checkengines = { "pdftex", "luatex" }
+recordstatus = true
 checkruns = 4
 -- vim: ts=2:sw=2:et:
