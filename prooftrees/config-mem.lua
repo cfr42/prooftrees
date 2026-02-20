c8a711ca7488e353634f40c838b66fb895ceaa76
Clea F. Rees <cfr@noreply.codeberg.org>
2026-02-20 23:36:54 +0000

    prooftrees : disable checks for memoization for xetex as we're not really checking anything and if we do, l3build ensures they fail

--- prooftrees/config-mem.lua
+++ prooftrees/config-mem.lua
@@ -1,5 +1,6 @@
--- $Id: config-mem.lua 11470 2026-01-14 21:23:24Z cfrees $
+-- $Id: config-mem.lua 11656 2026-02-20 23:36:38Z cfrees $
 -- Build configuration for prooftrees
 testfiledir = "testfiles-mem"
+checkengines = { "pdftex", "luatex" }
 checkruns = 3
 -- vim: ts=2:sw=2:et:
